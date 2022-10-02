73b95b45e8a36f5c73135a6527583de39a2e016a
letuu <albertvt@ya.ru>
2022-10-02 18:24:55 +0300

    num 3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("iwufehiwe");
         System.out.println("2");
+        System.out.println("3");
     }
 }
